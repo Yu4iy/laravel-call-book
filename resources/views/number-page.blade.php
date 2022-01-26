18c12e5a402b549b17a75b7b4666478f4325c03a
Yu4iy <yurimohyliuk@gmail.com>
2022-01-26 07:25:17 +0100

    FIX: style

--- resources/views/number-page.blade.php
+++ resources/views/number-page.blade.php
@@ -4,8 +4,14 @@
 	<section class="call-book">
 		<ol class="call-book__list">
 			<div class="container">
+				<li class="call-book__item label">
+					<span class="call-book__id">ID</span>
+					<span class="call-book__name">NAME</span>
+					<span class=call-book__phone>PHONE</span>
+					<span class="call-book__mail">MAIL</span>
+				</li>
 			@foreach ($numbers as $number)
-			<li class="call-book__item">
+				<li class="call-book__item">
 					<span class="call-book__id">{{$number->id}}</span>
 					<span class="call-book__name">{{$number->name}}</span>
 					<span class=call-book__phone><a href="tel:{{$number->phone}}">{{$number->phone}}</a></span>
