b751eb0ac79cc7f5c6ead7ef14e325a3102da578
Yu4iy <yurimohyliuk@gmail.com>
2022-01-26 07:15:58 +0100

    ADD: display

--- resources/views/number-page.blade.php
+++ resources/views/number-page.blade.php
@@ -3,47 +3,16 @@
 @section('content')
 	<section class="call-book">
 		<ol class="call-book__list">
+			<div class="container">
+			@foreach ($numbers as $number)
 			<li class="call-book__item">
-				<span class="call-book__id">1</span>
-				<span class="call-book__name">Paolo Duzioni</span>
-				<span class=call-book__phone><a href="tel:+74951234567">+7 (495) 123-45-67</a></span>
-				<span class="call-book__mail">LOREMIPSUM@mail.it</span>
-			</li>
-
-			<li class="call-book__item">
-				<span class="call-book__id">1</span>
-				<span class="call-book__name">Paolo Duzioni</span>
-				<span class=call-book__phone><a href="tel:+74951234567">+7 (495) 123-45-67</a></span>
-				<span class="call-book__mail">LOREMIPSUM@mail.it</span>
-			</li>
-
-			<li class="call-book__item">
-				<span class="call-book__id">1</span>
-				<span class="call-book__name">Paolo Duzioni</span>
-				<span class=call-book__phone><a href="tel:+74951234567">+7 (495) 123-45-67</a></span>
-				<span class="call-book__mail">LOREMIPSUM@mail.it</span>
-			</li>
-
-			<li class="call-book__item">
-				<span class="call-book__id">1</span>
-				<span class="call-book__name">Paolo Duzioni</span>
-				<span class=call-book__phone><a href="tel:+74951234567">+7 (495) 123-45-67</a></span>
-				<span class="call-book__mail">LOREMIPSUM@mail.it</span>
-			</li>
-
-			<li class="call-book__item">
-				<span class="call-book__id">1</span>
-				<span class="call-book__name">Paolo Duzioni</span>
-				<span class=call-book__phone><a href="tel:+74951234567">+7 (495) 123-45-67</a></span>
-				<span class="call-book__mail">LOREMIPSUM@mail.it</span>
-			</li>
-
-			<li class="call-book__item">
-				<span class="call-book__id">1</span>
-				<span class="call-book__name">Paolo Duzioni</span>
-				<span class=call-book__phone><a href="tel:+74951234567">+7 (495) 123-45-67</a></span>
-				<span class="call-book__mail">LOREMIPSUM@mail.it</span>
-			</li>
-		</ol>
+					<span class="call-book__id">{{$number->id}}</span>
+					<span class="call-book__name">{{$number->name}}</span>
+					<span class=call-book__phone><a href="tel:{{$number->phone}}">{{$number->phone}}</a></span>
+					<span class="call-book__mail">{{$number->mail}}</span>
+				</li>
+				@endforeach
+			</div>
+			</ol>
 	</section>
 @endsection
